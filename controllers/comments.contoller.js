7ec71854e973fee15d0add99d99dbf53f7ae720a
Sarah Baines <sarahbaines@sarahs-mbp-2.home>
2020-03-08 15:12:23 +0000

    completed delete article method

--- controllers/comments.contoller.js
+++ controllers/comments.contoller.js
@@ -13,7 +13,7 @@ exports.patchComment = (req, res, next) => {
 exports.deleteComment = (req, res, next) => {
   const { comment_id } = req.params;
   return removeComment(comment_id)
-    .then(response => {
+    .then(() => {
       res.status(204).send();
     })
     .catch(next);
